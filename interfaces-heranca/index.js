7e1845faa93c0e54b9c1897a36a8c6adc568f7f7
leobuck <leobuck12@gmail.com>
2022-11-02 10:49:37 -0300

    Super e sobrescrita

--- interfaces-heranca/index.js
+++ interfaces-heranca/index.js
@@ -10,6 +10,7 @@ contaCorrenteLeonardo.depositar(500);
 contaCorrenteLeonardo.sacar(100);
 
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+contaPoupanca.sacar(10);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteLeonardo);
