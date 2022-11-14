0a244a1ad433b7b9f4f246f5f4de90f438d66b1d
leobuck <leobuck12@gmail.com>
2022-11-14 16:53:24 -0300

    Interfaces

--- interfaces-heranca/SistemaAutenticacao.js
+++ interfaces-heranca/SistemaAutenticacao.js
@@ -1,5 +1,6 @@
+// Ser autenticavel significa ter o método autenticar
 export class SistemaAutenticacao {
-    static login(funcionario, senha) {
-        return funcionario.senha == senha;
+    static login(autenticavel, senha) {
+        return autenticavel.autenticar(senha);
     }
 }
