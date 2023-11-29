320419ec6b31f35d0559b35f7b1d72c58f9a80bc
leobuck <leobuck12@gmail.com>
2023-11-28 21:30:22 -0300

    Percorrendo uma lista

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -4,4 +4,9 @@ function tocaSomPom() {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador++;
+}
