c48ce603f426d8fe7887fb75b2f13b67c9ec53c5
leobuck <leobuck12@gmail.com>
2023-11-28 21:38:22 -0300

    Funções com parâmetros

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,5 +1,5 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idAudio) {
+    document.querySelector(idAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -7,6 +7,6 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = tocaSom;
     contador++;
 }
